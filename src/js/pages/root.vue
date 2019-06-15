2a5a96355e9fbb1015c3740f6e89b9ea61c691f9
Shantel Bosmer <sbosmer@oa-c2.21-school.ru>
2019-06-15 18:14:07 +0300

    killfry

--- src/js/pages/root.vue
+++ src/js/pages/root.vue
@@ -1,15 +1,30 @@
 <script>
+
 import LayoutDefault from '../layout/default.vue';
+import Navigation from '../layout/nav.vue';
+import '../../scss/root.scss';
+import '../../scss/nav.scss';
+
 
 export default {
   components: {
-    LayoutDefault
+    LayoutDefault,
+	Navigation
   }
 }
+
+
 </script>
 
 <template>
-<div>
-  <p>It's happening!</p>
-</div>
+<layout-default>
+	<navigation></navigation>
+	<div class="container">
+		<div class="row">
+			<div class="col bg-light justify-content-center main">
+				<h1 class="text-center">Хакатон 21</h1>
+			</div>
+		</div>
+	</div>
+</layout-default>
 </template>
